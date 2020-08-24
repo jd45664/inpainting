306e4be95e18ce77db018888941d65119b19e7f2
jd45664 <35519322+jd45664@users.noreply.github.com>
2020-08-24 15:04:02 -0500

    Update shape_predictor.py

--- shape_predictor.py
+++ shape_predictor.py
@@ -95,7 +95,9 @@ def align_face(filepath,predictor):
                max(pad[3] - img.size[1] + border, 0))
         if enable_padding and max(pad) > border - 4:
             pad = np.maximum(pad, int(np.rint(qsize * 0.3)))
-            img = np.pad(np.float32(img), ((pad[1], pad[3]), (pad[0], pad[2]), (0, 0)), 'reflect')
+            #img = np.pad(np.float32(img), ((pad[1], pad[3]), (pad[0], pad[2]), (0, 0)), 'median')
+            img = np.pad(np.float32(img), ((pad[1], pad[3]), (pad[0], pad[2]), (0, 0)), 'constant',
+            constant_values = np.mean(np.array(img)[0:3,0:3,0:3]))
             h, w, _ = img.shape
             y, x, _ = np.ogrid[:h, :w, :1]
             mask = np.maximum(1.0 - np.minimum(np.float32(x) / pad[0], np.float32(w - 1 - x) / pad[2]),
